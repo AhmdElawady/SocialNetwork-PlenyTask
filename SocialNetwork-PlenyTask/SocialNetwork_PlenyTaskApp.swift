f1fd4de6da838b2d869526a02fa8861527c30b11
AhmdElawady <a.elawady303@gmail.com>
2023-03-14 03:37:56 +0200

    first commit

---
+++ SocialNetwork-PlenyTask/SocialNetwork_PlenyTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialNetwork_PlenyTaskApp.swift
+//  SocialNetwork-PlenyTask
+//
+//  Created by Awady on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct SocialNetwork_PlenyTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
